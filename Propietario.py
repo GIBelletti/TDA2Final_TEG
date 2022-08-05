d6500985d9895caf2740a3c22c4c3265cb86542e
Belletti Gabriel Ignacio <44848915+GIBelletti@users.noreply.github.com>
2022-08-05 15:35:10 -0300

    fix clase propietario

--- Propietario.py
+++ Propietario.py
@@ -55,7 +55,8 @@ def obtener_jugadores_con_menos_paises(cantidad_otorgada, jugadores):
 
 
 def obtener_desenpate_por_mayor_dado(jugadores):
-    """Siguiendo las reglas del TEG genera un desenpate por los 2 paises restantes"""
+    """Siguiendo las reglas del TEG genera un desenpate por los 2 paises restantes.
+    Siempre son 2 los paises restantes."""
     valores = {1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
     maximo = 0
     for jugador in jugadores:
@@ -76,16 +77,19 @@ class Propietario_TEG:
         self.propietario = propietario
         self.ejercitos = 1
 
-    def propietario(self):
+    def propietario_actual(self):
         return self.propietario
 
-    def defender(self, atacante):
+    def cantidad_de_ejercitos(self):
+        return self.ejercitos
+
+    def defender(self, atacante, debug=False):
         """Retorna True si se logro defender, False si cambio de propietario"""
         cantidad_ejercito = atacante.ejercitos_de_ofensiva()
-        bajas = self._combate(cantidad_ejercito)
+        bajas = self._combate(cantidad_ejercito, debug)
         atacante.desplazar(bajas)
         if self.ejercitos == 0:
-            self.propietario = atacante.propietario()
+            self.propietario = atacante.propietario_actual()
             self.ejercitos = cantidad_ejercito - bajas
             atacante.desplazar(self.ejercitos)
             return False
@@ -97,10 +101,13 @@ class Propietario_TEG:
     def desplazar(self, cantidad):
         self.ejercitos -= cantidad
 
-    def _combate(self, cant_atacantes):
+    def _combate(self, cant_atacantes, debug=False):
         bajas_atacantes = 0
         dados_defensor = []
-        for i in range(self.ejercitos):
+        defensores = self.ejercitos
+        if defensores > 3:
+            defensores = 3
+        for i in range(defensores):
             dados_defensor.append(random.randint(1, 6))
         dados_atacante = []
         for i in range(cant_atacantes):
@@ -108,11 +115,17 @@ class Propietario_TEG:
         dados_defensor.sort(reverse=True)
         dados_atacante.sort(reverse=True)
         combatientes = min(len(dados_atacante), len(dados_defensor))
-        for combate in combatientes:
-            if dados_defensor[combate] < dados_atacante:
+        for combate in range(combatientes):
+            if dados_defensor[combate] < dados_atacante[combate]:
                 self.ejercitos -= 1
             else:
                 bajas_atacantes += 1
+        if debug:
+            print("Defensor: ", dados_defensor)
+            print("Atacante: ", dados_atacante)
+            print("Combatientes: ", combatientes)
+            print("bajas  def: ", combatientes - bajas_atacantes)
+            print("bajas  at: ", bajas_atacantes)
         return bajas_atacantes
 
     def puede_atacar(self):
@@ -125,4 +138,40 @@ class Propietario_TEG:
         return atacantes
 
     def es_limitrofe(self, otro):
-        return otro.propietario() != self.propietario
+        return otro.propietario_actual() != self.propietario
+
+# def pru():
+#     p1 = Propietario_TEG(PROPIETARIOS_COLORES[0])
+#     p2 = Propietario_TEG(PROPIETARIOS_COLORES[1])
+#     if p1.ejercitos != 1:
+#         print("error, ejercitos iniciales: ", p1.ejercitos)
+#     if p1.ejercitos_de_ofensiva() != 0:
+#         print("error, ejercitos ofensiva: ", p1.ejercitos)
+#     if p1.puede_atacar():
+#         print("error, ataque inicial")
+#     p1.reforzar(2)
+#     if p1.ejercitos != 3:
+#         print("error, ejercitos incrementados: ", p1.ejercitos)
+#     if p1.ejercitos_de_ofensiva() != 2:
+#         print("error, ejercitos ofensiva: ", p1.ejercitos)
+#     if not p1.puede_atacar():
+#         print("error, ataque afirmativo")
+#     p2.reforzar(5)
+#     if p2.cantidad_de_ejercitos() != 6:
+#         print("error, ejercitos incrementados p2: ", p1.ejercitos)
+#     if p2.ejercitos_de_ofensiva() != 3:
+#         print("error, ejercitos ofensiva p2: ", p1.ejercitos)
+#     p2.desplazar(1)
+#     if p2.cantidad_de_ejercitos() != 5:
+#         print("error, ejercitos desplazados p2: ", p1.ejercitos)
+#     print("fin")
+#     print("ejercito at", p1.cantidad_de_ejercitos())
+#     print("ejercito def", p2.cantidad_de_ejercitos())
+#     print(p2.cantidad_de_ejercitos())
+#     print(p2.defender(p1, True))
+#     print("ejercito at", p1.cantidad_de_ejercitos())
+#     print("ejercito def", p2.cantidad_de_ejercitos())
+#     print(p1.propietario_actual())
+#     print(p2.propietario_actual())
+
+# pru()
